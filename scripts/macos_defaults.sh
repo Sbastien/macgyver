e6e386662363c14a45937daca9dc65ed462752e2
Sébastien Loyer <sl@teeptrak.com>
2025-11-03 13:45:12 +0100

    feat: enable press & hold

--- scripts/macos_defaults.sh
+++ scripts/macos_defaults.sh
@@ -158,8 +158,8 @@ configure_input() {
     defaults write NSGlobalDomain KeyRepeat -int "$KEY_REPEAT_RATE"
     defaults write NSGlobalDomain InitialKeyRepeat -int "$KEY_REPEAT_DELAY"
 
-    # Disable press-and-hold for keys in favor of key repeat
-    defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+    # Enable press-and-hold for accented characters (é, è, ê, etc.)
+    defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool true
 
     # Enable full keyboard access for all controls (Tab in modal dialogs)
     defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
